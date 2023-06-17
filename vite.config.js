daffb8a74f756e6748087541ee2396e318808c2c
SydBal <dominic.balassone@gmail.com>
2023-06-16 21:27:02 -0700

    feat: removed typescript usage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Configure base to your GitHub repo name
-  base: "vite-gh-pages",
+    // Configure base to your GitHub repo name
+    base: "vite-gh-pages",
 })
